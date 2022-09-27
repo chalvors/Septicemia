88ad5ab91b6be6294f9232f4ba03c441bc67188c
cmullins00 <112650143+cmullins00@users.noreply.github.com>
2022-09-27 01:00:00 -0700

    Add files via upload

---
+++ Assets/Src/Conner/Scripts/enemyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyFunc();
+    }
+
+    // Update is called once per frame
+    private void enemyFunc()
+    {
+        print("Funky enemies coming soon!");
+    }
+}
