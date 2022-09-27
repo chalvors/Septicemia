071f044cf8265079888ed386a641577b62a0567f
hashbrown190 <hashbrown190@yahoo.com>
2022-09-26 23:10:06 -0700

    Added Sound Script

---
+++ Assets/Prefabs/Kyle/Sound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sound Coming Soon!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Sound Coming Soon!");
+    }
+}
