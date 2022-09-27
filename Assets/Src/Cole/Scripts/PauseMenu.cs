2fea42dc42b864e10532a1f1ef9762019dcfac6f
Cole Halvorson <darthcole28@gmail.com>
2022-09-27 00:18:01 -0700

    update pause menu script for mvp

--- Assets/Src/Cole/Scripts/PauseMenu.cs
+++ Assets/Src/Cole/Scripts/PauseMenu.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    void Pause() {
-        Debug.Log("Mista White I'm Pausin");
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    public void Pause() {
+        Debug.Log("Yo Mista White I'm Pausin");
     }
 }
