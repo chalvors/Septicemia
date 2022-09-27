d0f14e22b2bd93ffac5e8256a84e215da096a86b
Cole Halvorson <darthcole28@gmail.com>
2022-09-26 22:54:26 -0700

    added pause menu script for mvp

---
+++ Assets/Src/Cole/Scripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Pause Menu Called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
