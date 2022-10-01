b4e2747e2b86d59d4679a545ee4a334fe270bc53
Cole Halvorson <darthcole28@gmail.com>
2022-10-01 14:30:38 -0700

    working back buttons

---
+++ Assets/Src/Cole/Scripts/SettingsMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingsMenu;
+
+    public void Back() {
+        Debug.Log("heading back to main menu from settings menu");
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
