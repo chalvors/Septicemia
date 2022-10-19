1d5c48393eef4ecb97bda7b575f2a9141453fe26
hashbrown190 <hashbrown190@yahoo.com>
2022-10-19 15:21:10 -0700

    Added my stress test scene and the stress test presentation

---
+++ Assets/Tst/Kyle/Stress Test/Boss_Stress.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Stress : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject swarmerPrefab;
+
+    [SerializeField]
+    private float swarmerInterval = 0.5f;
+
+    public GameObject prefab;
+    public Transform player;
+    //public GameObject.find("Player");
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnBoss(swarmerInterval, swarmerPrefab));
+
+
+
+
+    }
+
+
+    private IEnumerator spawnBoss(float interval, GameObject boss)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newBoss = Instantiate(prefab, new Vector2(Random.Range(-9, 9), Random.Range(-4, 4)), Quaternion.identity);
+
+        StartCoroutine(spawnBoss(swarmerInterval, swarmerPrefab));
+        //player = gameObject.transform;
+        //newBoss.GetComponent("Player");
+        //newBoss = GameObject.AddComponent<Player>();
+
+
+
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
