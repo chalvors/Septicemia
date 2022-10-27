97d1c9ba3ea9f763d27ad992d4cdfde6fcb9d400
cmullins00 <connermullins00@gmail.com>
2022-10-26 17:50:56 -0700

    Worked on implementing my decorator pattern

---
+++ Assets/Src/Conner/Scripts/BaseEnemy.cs
@@ -0,0 +1,71 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using Debug = System.Diagnostics.Debug;
+
+namespace Enemies
+{
+
+    public interface IEnemy
+    {
+        string GetDetails();
+    }
+
+    public class BaseEnemy : IEnemy
+    {
+        public string GetDetails() => "Melee attacks";
+    }
+
+    public abstract class EnemyTypes : IEnemy
+    {
+        private readonly IEnemy _enemy;
+
+        public EnemyTypes (IEnemy enemy)
+        {
+            _enemy = enemy;
+        }
+
+        public virtual string GetDetails() => _enemy.GetDetails();
+    }
+
+    public class RifleEnemy : EnemyTypes
+    {
+        public RifleEnemy(IEnemy enemy) : base(enemy)
+        {
+
+        }
+        public override string GetDetails() => "Rifle attacks";
+    }
+
+    public class PistolEnemy : EnemyTypes
+    {
+        public PistolEnemy(IEnemy enemy) : base(enemy)
+        {
+
+        }
+        public override string GetDetails() => "Pistol attacks";
+    }
+
+    public class TankEnemy : EnemyTypes
+    {
+        public TankEnemy(IEnemy enemy) : base(enemy)
+        {
+
+        }
+        public override string GetDetails() => base.GetDetails() + " and more health";
+    }
+
+    public class EnemyCreator
+    {
+        public static void UpgradeEnemy()
+        {
+            var basicEnemy = new BaseEnemy();
+            EnemyTypes upgraded = new RifleEnemy(basicEnemy);
+
+            Debug.WriteLine($"Enemy: '{upgraded.GetDetails()}'");
+        }
+    }
+}
