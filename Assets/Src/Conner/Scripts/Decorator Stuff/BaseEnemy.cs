fb9360eb780761e30de7850bee4432da0bd038a2
cmullins00 <connermullins00@gmail.com>
2022-11-02 00:16:55 -0700

    Enemies no longer appear on top of the menu. Enemies that stop now face the correct direction when pathfinding. Enemy health upgrade now gets the correct variable

--- Assets/Src/Conner/Scripts/Decorator Stuff/BaseEnemy.cs
+++ Assets/Src/Conner/Scripts/Decorator Stuff/BaseEnemy.cs
@@ -6,6 +6,11 @@ using static UnityEngine.GraphicsBuffer;
 
 public class BaseEnemy : Enemy
 {
+    [SerializeField]
+    private Transform target;
+
+    bool isMoving = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,26 @@ public class BaseEnemy : Enemy
         damage = 0;
 
         //IncreaseHealth();
+    }
 
+    private void FixedUpdate()
+    {
+        if (target != null && isMoving == false)
+        {
+            Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+            rotation *= Quaternion.Euler(90, 0, 0);
+            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        }
+        StartCoroutine(moveCheck());
+    }
+
+    IEnumerator moveCheck()
+    {
+        var p1 = transform.parent.position;
+        yield return new WaitForSeconds(.5f);
+        var p2 = transform.parent.position;
+
+        isMoving = (p1 != p2);
     }
 
     public override int GetDamage()
@@ -23,6 +47,6 @@ public class BaseEnemy : Enemy
 
     public override int GetHealth()
     {
-        return damage;
+        return health;
     }
 }
