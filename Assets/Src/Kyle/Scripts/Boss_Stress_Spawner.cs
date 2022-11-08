c6d11566d1c4e70366cad118ab0d5ed3145186de
hashbrown190 <hashbrown190@yahoo.com>
2022-11-08 12:15:20 -0800

    Added a new Boss spawner script, still working on it though

--- Assets/Src/Kyle/Scripts/Boss_Stress_Spawner.cs
+++ Assets/Src/Kyle/Scripts/Boss_Stress_Spawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss_Spawner : MonoBehaviour
+public class Boss__Stress_Spawner : MonoBehaviour
 {
 
     [SerializeField]
@@ -10,38 +10,38 @@ public class Boss_Spawner : MonoBehaviour
 
     [SerializeField]
     private float swarmerInterval = 0.5f;
-    
+
     public GameObject prefab;
     public Transform player;
     //public GameObject.find("Player");
 
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnBoss(swarmerInterval, swarmerPrefab));
-       
+
     }
 
 
     private IEnumerator spawnBoss(float interval, GameObject boss)
     {
         yield return new WaitForSeconds(interval);
-        //GameObject Iboss boss = new Boss();
-        GameObject newBoss = Instantiate(prefab, new Vector2(Random.Range(-20,-14),Random.Range(-4, -8)), Quaternion.identity);
 
-         StartCoroutine(spawnBoss(swarmerInterval, swarmerPrefab));
+        GameObject newBoss = Instantiate(prefab, new Vector2(Random.Range(-20, -14), Random.Range(-4, -8)), Quaternion.identity);
+
+        StartCoroutine(spawnBoss(swarmerInterval, swarmerPrefab));
         //player = gameObject.transform;
         //newBoss.GetComponent("Player");
         //newBoss = GameObject.AddComponent<Player>();
-       
-        
-        
+
+
+
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
