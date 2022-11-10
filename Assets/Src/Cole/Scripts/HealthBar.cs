a4a473f694e16d204b7dced0591bc88771ae405b
Cole Halvorson <darthcole28@gmail.com>
2022-11-09 20:20:43 -0800

    finished health bar and script

--- Assets/Src/Cole/Scripts/HealthBar.cs
+++ Assets/Src/Cole/Scripts/HealthBar.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
     public Slider slider;
-
+    
     public void setMaxHealth(int health) {
         slider.maxValue = health;
         slider.value = health;
