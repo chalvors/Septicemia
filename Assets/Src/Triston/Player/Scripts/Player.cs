7d63a7620ec2c295e049d60dd6f1c1b8e0a5824e
Triston Hardcastle Peck <cooldude101triston@gmail.com>
2022-11-09 16:53:00 -0800

    added attack collision and damage

--- Assets/Src/Triston/Player/Scripts/Player.cs
+++ Assets/Src/Triston/Player/Scripts/Player.cs
@@ -14,6 +14,11 @@ public class Player : MonoBehaviour
     public Animator animator;
     public float delay = 0.3f;
     private bool attackBlocked;
+
+    [SerializeField]
+    private Transform circleOrigin;
+    [SerializeField]
+    private float radius;
     
     public void heal() 
     {
@@ -79,5 +84,32 @@ public class Player : MonoBehaviour
         }
     }
 
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 position = circleOrigin == null ? Vector3.zero : circleOrigin.position;
+        Gizmos.DrawWireSphere(position, radius);
+    }
+
+    public void DetectColliders()
+    {
+
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(circleOrigin.position,radius))
+        {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                continue;
+            }
+
+            BaseEnemy enemy;
+
+            if (enemy = collider.GetComponent<BaseEnemy>())
+            {
+                Debug.Log(collider.name);
+                //enemy.TakeDamage(stats.GetDamage());
+            }
+        }
+    }
 
 }
