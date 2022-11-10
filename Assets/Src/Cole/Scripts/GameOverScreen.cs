708571dd4b28eb68cda0597beca0517ac8aac600
Cole Halvorson <darthcole28@gmail.com>
2022-11-09 21:02:13 -0800

    added game over screen

---
+++ Assets/Src/Cole/Scripts/GameOverScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("restarting the game");
+    }
+}
